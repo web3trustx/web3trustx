939cd6f75c19ebb340af3b602c80164b2eae004e
Web3TrustX <contact@web3trustx.com>
2025-11-05 15:25:51 +0100

    Change default language to English

--- lib/LanguageContext.tsx
+++ lib/LanguageContext.tsx
@@ -12,7 +12,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
-  const [language, setLanguage] = useState<Language>('es');
+  const [language, setLanguage] = useState<Language>('en');
 
   return (
     <LanguageContext.Provider
